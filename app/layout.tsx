712703fe1b0aef396fecd49bfc17faa4bb6bfbad
connorkuljis <conkuljis@gmail.com>
2022-11-12 23:43:57 +0800

    init app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
 import './globals.css'
+import { Syne } from '@next/font/google'
+
+const syne = Syne()
 
 export default function RootLayout({
   children,
@@ -12,7 +15,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={syne.className}>{children}</body>
     </html>
   )
 }
