504359e1b1810c1289f8ae0fe108008b365d4a46
connorkuljis <conkuljis@gmail.com>
2022-11-14 02:39:56 +0800

    nav and css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Syne } from '@next/font/google'
+import styles from './page.module.css'
+import Link from 'next/link'
 
 const syne = Syne()
 
@@ -15,7 +17,23 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className={syne.className}>{children}</body>
+
+			<body className={syne.className}>
+				<header className={styles.container}>
+
+					<nav>
+						<h1>
+							<Link className={styles.nav} href="/">Connor Kuljis</Link>
+							<Link className={styles.nav} href="/blog">Blog</Link>
+							<Link className={styles.nav} href="/guestbook">Guest Book</Link>
+						</h1>
+					</nav>
+
+				</header>
+
+				<div>{children}</div>
+
+			</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import styles from './page.module.css'
 export default function Home() {
 	return (
 		<div className={styles.container}>
-			<h1>Connor Kuljis</h1>
 			<main>hello</main>
 		</div>
 	)
