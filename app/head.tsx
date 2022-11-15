36205594a4817c4a9d7679f02f7d4f271f8aa4d4
connorkuljis <conkuljis@gmail.com>
2022-11-16 01:27:18 +0800

    title

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>Create Next App</title>
+      <title>Connor Kuljis</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="connor-kuljis" />
+      <meta name="description" content="connor-kuljis portfolio site" />
       <link rel="icon" href="/favicon.ico" />
     </>
   )
